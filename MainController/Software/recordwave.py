f41062082d8ec406a35fc11286db2b7b31fe44a0
Patrick van Beem <Patrick@vanBeem.info>
2019-02-01 22:33:38 +0100

    Added SmartWares recognition

--- MainController/Software/recordwave.py
+++ MainController/Software/recordwave.py
@@ -9,6 +9,8 @@ class recorder:
     MAXRECORD = 500
     data = array.array('L', (0,) * MAXRECORD)
     pin = Pin(5)
+    led = Pin(2)
+    led.init(Pin.OUT)
     last_timestamp = time.ticks_us()
     counter = 0
     recording = False
@@ -22,15 +24,34 @@ class recorder:
         code = 0
         for count in range(self.counter - 50, self.counter - 2, 2):
             if self.data[count] <= cutof and self.data[count+1] > cutof:
-               # zero
                code <<= 1
+               code |= 1
             elif self.data[count] > cutof and self.data[count+1] <= cutof:
+               code <<= 1
+            else:
+                #print("Elro failed", cutof, count, self.data[count], self.data[count+1])
+                return False
+        print("Decoded: {0} / 0x{0:x} / {0:024b}".format(code))
+        return True
+
+    def decodeSmartWares(self):
+        if (self.counter < 131  
+            or self.data[self.counter - 131] > 3000 or self.data[self.counter - 131] < 2000):
+            return False
+        short = self.data[self.counter - 2] # The short as part of the end-sync
+        cutof = short * 2
+        code = 0
+        code1 = 0
+        for count in range(self.counter - 130, self.counter - 2, 2):
+            if self.data[count] <= cutof and self.data[count+1] <= cutof:
+               code <<= 1
+            elif self.data[count] <= cutof and self.data[count+1] > cutof:
                code <<= 1
                code |= 1
             else:
-                print("failed", cutof, count, self.data[count], self.data[count+1], self.data[count+2], self.data[count+3])
+                #print("Smartwares failed", cutof, count, self.data[count], self.data[count+1])
                 return False
-        print("Decoded: {0} / 0x{0:x} / {0:b}".format(code))
+        print("Decoded: {0} / 0x{0:16x} / {0:064b}".format(code))
         return True
 
     def rxinterrupt(self, pin):
@@ -39,16 +60,21 @@ class recorder:
         self.last_timestamp = timestamp
 
         # Pulses shorter than 250 are usually noise. Though it might happen...
-        if duration > 250:
+        if duration > 300 or (duration > 225 and self.counter > 0):
             self.data[self.counter] = duration
             self.counter += 1
-            if duration > 5000 or self.counter == self.MAXRECORD:
-                if self.counter < 10:
+            if duration > 7000 or self.counter == self.MAXRECORD:
+                self.led.value(not self.led.value())
+                if self.counter < 50:
+                    #print(self.counter, self.data[0:self.counter])
+                    pass
+                elif self.decodeSmartWares():
                     pass
                 elif self.decodeElro():
                     pass
                 else:
-                    print(self.counter, self.data[0:self.counter])
+                    pass
+                    #print(self.counter, self.data[0:self.counter])
                 self.counter = 0
         else:
             self.counter = 0
