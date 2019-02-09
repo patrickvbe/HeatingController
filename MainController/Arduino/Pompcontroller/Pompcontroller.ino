78395a3f58fdb0684917f795960abced70df0a47
Patrick van Beem <Patrick@vanBeem.info>
2019-02-09 01:01:07 +0100

    Got the 433MHz working both sending and receiving on Arduino ESP.

--- MainController/Arduino/Pompcontroller/Pompcontroller.ino
+++ MainController/Arduino/Pompcontroller/Pompcontroller.ino
@@ -1,27 +1,33 @@
-struct Protocol {
-    byte signallength;
-    byte relevantlength;
-    unsigned int start_high;
-    unsigned int start_low,
-    unsigned int zero_high;
-    unsigned int zero_low;
-    unsigned int one_high;
-    unsigned int one_low;
-    unsigned int end_high
-    unsigned int end_low;
-}
 
-const int SMARTWARES     = 0;
+
+struct Protocol
+{
+  byte signallength;
+  byte relevantlength;
+  unsigned int start_high;
+  unsigned int start_low;
+  unsigned int zero_high;
+  unsigned int zero_low;
+  unsigned int one_high;
+  unsigned int one_low;
+  unsigned int end_high;
+  unsigned int end_low;
+};
+
+const int SMARTWARES = 0;
 const int WEATHERSTATION = 1;
-const int CONRADRSL      = 2;
-const int ELRO           = 3;
+const int CONRADRSL = 2;
+const int ELRO = 3;
 
 const Protocol PROTOCOLS[] = {
-    //Protocol(SMARTWARES,    132, 130, 300, 2500, 300,  300,  300, 1200, 300, 10000),     // Smartwares (Action)
-    {Protocol(130, 128,   0,    0, 300,  300,  300, 1200, 300, 10000)},   // Smartwares (Action)
-    {Protocol(74,  70,   0,    0, 525,  925,  525, 1850, 550,  3850)},   // Weatherstation
-    {Protocol(66,  64,   0,    0, 600, 1200, 1200,  600, 600,  7000)},   // Conrad RSL
-    {Protocol(50,  48,   0,    0, 325,  975,  975,  325, 325, 10000)}    // Elro
+    //Protocol{132, 130, 300, 2500, 300,  300,  300, 1200, 300, 10000},     // Smartwares (Action)
+    Protocol{130, 128, 0, 0, 300,  300,  300, 1200, 300, 10000},    // Smartwares (Action)
+    Protocol{ 74,  70, 0, 0, 500,  950,  500, 1950, 550,  3850},    // Weatherstation
+    Protocol{ 66,  64, 0, 0, 600, 1200, 1200,  600, 600,  7000},    // Conrad RSL
+    //Protocol{ 50,  48, 0, 0, 300, 1000, 1000,  300, 300, 10000}     // Elro
+    Protocol{ 50,  48, 0, 0, 400,  900, 1050,  250, 400, 10100}     // Elro
+/*
+*/
 };
 
 // The first threshold is when we start recording, the second is the smalles valid value while recording.
@@ -30,174 +36,267 @@ const Protocol PROTOCOLS[] = {
 // MAXRECORD is the maximum buffer size.
 // const unsigned long DURATIONTHRESHOLD1 = 275;
 // const unsigned long DURATIONTHRESHOLD2 = 225;
-const unsigned long DURATIONTHRESHOLD1 = 250;
+const unsigned long DURATIONTHRESHOLD1 = 275;
 const unsigned long DURATIONTHRESHOLD2 = 225;
-const unsigned long SYNCPULSETHRESHOLD =3000;
-const int           MAXRECORD 150;
+const unsigned long SYNCPULSETHRESHOLD = 3000;
+const int MAXRECORD = 150;
 
 // weatherstation format: 1111111 000000010100101 111110011011, middle part is temperatur in 10ths C
-int getTempFromWeather(const int weather):
+int getTempFromWeather(const unsigned long weather)
 {
-    return (weather >> 12) & 0xFFF;
+  int temp = (weather >> 12) & 0xFFF;
+  if ( temp & 0x800 ) temp |= 0xF000; // Add missing negative bits.
+  return temp;
 }
 
-class sender {
-  private:
-    int pin;
+class sender
+{
+public:
+  int pin;
 
-  public:  
-    void sender(const int pin)
-    {
-        this.pin = pin;
-        // Decouple...
-        pinMode(this.pin, INPUT);
-    }
+  sender(const int sndpin)
+  {
+    this->pin = sndpin;
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+  }
 
-    void send(const byte protocol_id, const unsigned long code, int repeat = 3)
+  void send(const int protocol_id, const unsigned long code, int repeat = 6)
+  {
+    const Protocol &protocol = PROTOCOLS[protocol_id];
+    while (repeat-- > 0)
     {
-        const Protocol& protocol = PROTOCOLS[protocol_id];
-        while (repeat-- > 0)
+      int bitnr = (protocol.signallength / 2 - 1);
+      unsigned long bitmask = 1 << (bitnr - 1);
+      while (bitnr-- > 0)
+      {
+        digitalWrite(pin, HIGH);
+        if (code & bitmask)
         {
-            pinMode(this.pin, OUTPUT);
-            int bitnr = (protocol.signallength); // 2 - 1
-            unsigned long bitmask = 1 << (bitnr - 1);
-            while (bitnr-- > 0)
-            {
-                digitalWrite(this.pin, HIGH);
-                if (code & bitmask == 0)
-                {
-                    delayMicroseconds(protocol.zero_high);
-                    digitalWrite(this.pin, LOW);
-                    delayMicroseconds(protocol.zero_low);
-                }
-                else
-                {
-                    delayMicroseconds(protocol.one_high);
-                    digitalWrite(this.pin, LOW);
-                    delayMicroseconds(protocol.one_low);
-                }
-                bitmask >>= 1
-            }
-            digitalWrite(this.pin, HIGH);
-            delayMicroseconds(protocol.end_high);
-            digitalWrite(this.pin, LOW);
-            delayMicroseconds(protocol.end_low);
-            // Decouple...
-            pinMode(this.pin, INPUT);
-            if (repeat != 0) delay(10);
+          delayMicroseconds(protocol.one_high);
+          digitalWrite(pin, LOW);
+          delayMicroseconds(protocol.one_low);
         }
+        else
+        {
+          delayMicroseconds(protocol.zero_high);
+          digitalWrite(pin, LOW);
+          delayMicroseconds(protocol.zero_low);
+        }
+        bitmask >>= 1;
+      }
+      digitalWrite(pin, HIGH);
+      delayMicroseconds(protocol.end_high);
+      digitalWrite(pin, LOW);
+      delayMicroseconds(protocol.end_low);
     }
-}
+  }
+};
 
+/* Receive 433MHz codes. Although everything is packed in a class, the interrupt code
+   basically allows only one instance (why would you use multiple anyway?)
+*/
 class receiver
 {
-  public:
-    typedef void (received_callback*)(int protocol, unsigned long code, unsigned long timestamp);
+public:
 
-  private:
-    int                 pin;
-    int                 counter;
-    unsigned long       data[MAXRECORD];
-    unsigned long       last_timestamp;
-    received_callback   callback;
-    static receiver*    me = nullptr;
-    
-  public:
 
-    void receiver(int pin, received_callback callback = nullptr)
+  typedef void(*received_callback)(int protocol, unsigned long code, unsigned long timestamp);
+  int pin;
+  // This is where the code will be (usefull if you don't use the callback)
+  unsigned long code = 0;
+  unsigned long code_timestamp = 0;
+  int           code_protocol = -1;
+
+private:
+  int counter;
+  unsigned long data[MAXRECORD];
+  unsigned long last_timestamp;
+  received_callback callback;
+  static receiver* me;  // No arguments allowed for the interrupt, so: it's me!
+
+public:
+  receiver(const int recpin, received_callback reccallback = 0)
+  {
+    last_timestamp = micros();
+    counter = 0;
+    callback = reccallback;
+    pin = recpin;
+    pinMode(pin, INPUT);
+    receiver::me = this;
+  }
+
+  static void _read_interrupt()
+  {
+    receiver::me->read_interrupt();
+  }
+
+  void start()
+  {
+    last_timestamp = micros();
+    counter = 0;
+    attachInterrupt(pin, _read_interrupt, CHANGE);
+  }
+
+  void stop()
+  {
+    detachInterrupt(pin);
+  }
+
+private:
+  bool decode()
+  {
+    for (int idx = 0; idx < sizeof(PROTOCOLS); idx++)
     {
-        this.last_timestamp = micros();
-        this.counter = 0;
-        this.callback = callback;
-        this.pin = pin;
-        pinMode(this.pin, INPUT);
-        reciever.me = this;
+      if (decodeprotocol(idx))
+      {
+        return true;
+      }
     }
+    return false;
+  }
 
-    void start()
+  bool decodeprotocol(const int protocol_id)
+  {
+    const Protocol& protocol = PROTOCOLS[protocol_id];
+    if (counter < protocol.signallength)
+      return false;
+    int start = counter - protocol.signallength;
+    if (protocol.start_low != 0)
     {
-        this.last_timestamp = micros();
-        this.counter = 0;
-        attachInterrupt(this.pin, read_interrupt, CHANGE);
-    }
-
-    void stop()
-    {
-        detachInterrupt(this.pin);        
-    }
-
-  private:
-    bool decode()
-    {
-        for (idx = 0; idx < sizeof(PROTOCOLS); idx++)
-            if (this.decodeprotocol(idx)) return true;
+      if (data[start + 1] < (protocol.start_low * 2) / 3)
+      {
         return false;
+      }
+      start += 2;
     }
-
-    bool decodeprotocol(const int protocol_id)
+    if (protocol.end_low < (data[counter - 1] * 2) / 3)
     {
-        const Protocol* protocol = PROTOCOLS[protocol_id];
-        if ( this.counter < protocol.signallength ) return false;
-        int start = this.counter - protocol.signallength;
-        if ( protocol.start_low != 0 )
-        {
-            if ( this.data[start + 1] < (protocol.start_low * 2) / 3 )
-                return false;
-            start += 2;
-        }
-        if ( protocol.end_low < (this.data[this.counter - 1] * 2) / 3 )
-            return false;
-        // pre-calculate ranges for speed...
-        unsigned long zerohl = (protocol.zero_high) / 2;
-        unsigned long zerohh = zerohl * 3;
-        unsigned long zeroll = (protocol.zero_low) / 2;
-        unsigned long zerolh = zeroll * 3;
-        unsigned long onehl = (protocol.one_high) / 2;
-        unsigned long onehh = onehl * 3;
-        unsigned long onell = (protocol.one_low) / 2;
-        unsigned long onelh = onell * 3;
-        unsigned long code = 0;
-        for pos in range(start, start + protocol.relevantlength, 2):
-            high = this.data[pos]
-            low = this.data[pos+1]
-            if zerohl < high and  high < zerohh and zeroll < low and low < zerolh:
-                code <<= 1
-            elif onehl < high and  high < onehh and onell < low and low < onelh:
-                code <<= 1
-                code |= 1
-            else:
-                return False
-        if code == 0:
-            return False
-        // Pass it to the user.
-        this.code = code
-        this.code_timestamp = this.last_timestamp
-        this.code_protocol = protocol.id
-        if this._callback:
-            this._callback(this)
-        return True
+      return false;
     }
-
-    static void read_interrupt(this, pin)
+    // pre-calculate ranges for speed...
+    unsigned long zerohl = (protocol.zero_high) / 2;
+    unsigned long zerohh = zerohl * 3;
+    unsigned long zeroll = (protocol.zero_low) / 2;
+    unsigned long zerolh = zeroll * 3;
+    unsigned long onehl = (protocol.one_high) / 2;
+    unsigned long onehh = onehl * 3;
+    unsigned long onell = (protocol.one_low) / 2;
+    unsigned long onelh = onell * 3;
+    unsigned long code = 0;
+    int end = start + protocol.relevantlength;
+    for (int pos = start; pos < end; pos += 2)
     {
-        receiver* myself = receiver::me;
-        timestamp = micros()
-        duration = time.ticks_diff(timestamp, myself.last_timestamp)
-        myself.last_timestamp = timestamp
-
-        // Pulses shorter than 250us are usually noise. But my smartwares remote drifts to almost 225us...
-        if ( duration > DURATIONTHRESHOLD1 or (duration > DURATIONTHRESHOLD2 and myself.counter > 0) )
-        {
-            myself.data[this.counter] = duration
-            myself.counter += 1
-            if duration > SYNCPULSETHRESHOLD or myself.counter == MAXRECORD
-            {
-                // this.led.value(not this.led.value())
-                myself.decode();
-                myself.counter = 0
-            }
-        }
-        else:
-            this.counter = 0
+      unsigned long high = data[pos];
+      unsigned long low = data[pos + 1];
+      if (zerohl < high && high < zerohh && zeroll < low && low < zerolh)
+      {
+        code <<= 1;
+      }
+      else if (onehl < high && high < onehh && onell < low && low < onelh)
+      {
+        code <<= 1;
+        code |= 1;
+      }
+      else
+      {
+        return false;
+      }
     }
+    if (code == 0)
+    {
+      return false;
+    }
+    // Pass it to the user.
+    code = code;
+    code_timestamp = last_timestamp;
+    code_protocol = protocol_id;
+    if (callback)
+    {
+      callback(protocol_id, code, code-code_timestamp);
+    }
+    return true;
+  }
+
+  void read_interrupt()
+  {
+    unsigned long timestamp = micros();
+    unsigned long duration = timestamp - last_timestamp;
+    last_timestamp = timestamp;
+
+    // Pulses shorter than 250us are usually noise. But my smartwares remote drifts to almost 225us...
+    if (duration > DURATIONTHRESHOLD1 or (duration > DURATIONTHRESHOLD2 and counter > 0))
+    {
+      data[counter] = duration;
+      counter += 1;
+      if (duration > SYNCPULSETHRESHOLD or counter == MAXRECORD)
+      {
+        if (counter > 10)
+        {
+          /*
+          Serial.print(counter);
+          Serial.print(": ");
+          for (int idx=0; idx < counter; idx++)
+          {
+            Serial.print(data[idx]);
+            Serial.print(", ");
+          }
+          Serial.println();
+          //*/
+          decode();
+        }
+        counter = 0;
+      }
+    }
+    else
+    {
+      counter = 0;
+    }
+  }
+};
+
+receiver* receiver::me = 0;
+
+//--------------------------------------------------
+
+void code_received(int protocol, unsigned long code, unsigned long timestamp)
+{
+  Serial.print("Protocol: ");
+  Serial.print(protocol);
+  Serial.print(", code:");
+  Serial.print(code, BIN);
+  Serial.print(" / ");
+  Serial.print(code, HEX);
+  if ( protocol == WEATHERSTATION)
+  {
+    Serial.print(", T: ");
+    Serial.print(getTempFromWeather(code));
+  }
+  Serial.println();
+}
+
+receiver rcv(5, code_received);
+sender   snd(4);
+
+void setup()
+{
+  Serial.begin(230400);
+  rcv.start();
+}
+
+void loop()
+{
+  int cin = Serial.read();
+  if (cin == '1')
+  {
+    rcv.stop();
+    snd.send(ELRO, 0x144551);
+    rcv.start();
+  }
+  else if (cin == '0')
+  {
+    rcv.stop();
+    snd.send(ELRO, 0x144554);
+    rcv.start();
+  }
 }
